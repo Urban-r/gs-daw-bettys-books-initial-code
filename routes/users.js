049230b009586c4710960a85c85878ac19e090a7
Riyaad Shahid <126833781+Urban-r@users.noreply.github.com>
2024-10-09 14:38:24 +0100

    pc changes to laptop

--- routes/users.js
+++ routes/users.js
@@ -13,27 +13,24 @@ router.get('/register', function (req, res, next) {
 router.post('/registered', function (req, res, next) {
     // saving data in database
     const plainPassword = req.body.password
-    let sqlquery = "INSERT INTO users (firstname, lastname,username, hashedpassword) VALUES (?,?,?)";
+    let sqlquery = "INSERT INTO users (firstname, lastname, username, email, hashedpassword) VALUES (?,?,?,?,?)";
     bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
         // Store hashed password in your database.
-        db.query(err, result => {
+        let newrecord = [
+            req.body.firstname,
+            req.body.lastname,
+            req.body.username,
+            req.body.email,
+            hashedPassword
+        ]
+        db.query(sqlquery, newrecord, err, result => {
             if (err) {
                 next(err)
             }
-            let newrecord = [
-                req.body.firstname,
-                req.body.lastname,
-                req.body.username,
-                req.body.email,
-                hashedPassword
-            ]
-
-      })
-      res.send(' This user is added to database, name: '+ req.body.firstmame + ' lastname '+ req.body.lastname + 'username' + req.body.username + 'email' + req.body.email + 'hashedpasssword' + req.body.hashedPassword)
-})
-      
-
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
+            else      
+            res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                       
+        })    
+    })
 })
 
 // Export the router object so index.js can access it
